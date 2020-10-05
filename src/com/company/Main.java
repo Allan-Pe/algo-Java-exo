5daf2b18e0d86cb7919dd518efa46503fd9806fe
Pereira Allan <allan76440@gmail.com>
2020-10-05 16:29:51 +0200

    1ere version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    	// Déclaration TB
+		int [] scores = new int [7];
+		//Affectation valeur
+		scores[0] = 22;
+		scores[1] = 17;
+		scores[2] = 44;
+		scores[3] = 66;
+		scores[4] = 12;
+		scores[5] = 11;
+		scores[6] = 21;
+
+
+
+		int moyenne =0;
+		for (int m = 0 ; m< scores.length;m++ ) {
+
+			int s = scores[m];
+
+			moyenne = moyenne + s;
+
+		}
+		System.out.println(moyenne/ scores.length);
+
     }
 }
